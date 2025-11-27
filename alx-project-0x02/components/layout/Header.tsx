89026faf551309ccd7ea0e771f8430fb1f8c7aa6
grace <xperttechsolutions111@gmail.com>
2025-11-27 12:07:23 +0100

    Add basic routing: home and about pages + update Header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,26 +4,19 @@ export default function Header() {
   return (
     <header className="bg-white shadow-md">
       <div className="max-w-4xl mx-auto px-4 py-4 flex items-center justify-between">
-        {/* Logo / Title */}
+        
         <Link href="/" className="text-xl font-semibold">
           ALX Project
         </Link>
 
-        {/* Navigation */}
         <nav className="space-x-4">
-          <Link href="/" className="hover:underline">
-            Home
-          </Link>
-          <Link href="/posts" className="hover:underline">
-            Posts
-          </Link>
-          <Link href="/users" className="hover:underline">
-            Users
-          </Link>
-          <Link href="/about" className="hover:underline">
-            About
-          </Link>
+          <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/posts" className="hover:underline">Posts</Link>
+          <Link href="/users" className="hover:underline">Users</Link>
+          <Link href="/about" className="hover:underline">About</Link>
+          <Link href="/home" className="hover:underline">Home Page</Link>
         </nav>
+
       </div>
     </header>
   );
